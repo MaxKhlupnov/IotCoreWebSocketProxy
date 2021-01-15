2e653c3a276c8821e91c4232218fcfad1fda6cfe
khlupnov <m.khlupnov@gmail.com>
2021-01-15 19:54:54 +0300

    INitial SignalR hub

---
+++ Hub/SignalRConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IotCoreWebSocketProxy.Hub
+{
+    public class SignalRConnection
+    {
+        public string DeviceId { get; set; }
+        public string DevicePwd { get; set; }
+        public string DeviceCert { get; set; }
+    }
+}
